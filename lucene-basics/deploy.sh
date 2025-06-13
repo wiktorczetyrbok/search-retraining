2ee1ec03cc35b4e183e0a2dd764fb899192c4653
wiktorczetyrbok <kasztanators2701@gmail.com>
2025-06-13 10:42:56 +0200

    deployment scripts

--- lucene-basics/deploy.sh
+++ lucene-basics/deploy.sh
@@ -7,18 +7,18 @@ SERVICE_NAME="wczetyrbok-lucene-mini-search"
 IMAGE_TAG="0.1"
 IMAGE_URI="${REGION}-docker.pkg.dev/${PROJECT_ID}/${REPO}/${SERVICE_NAME}:${IMAGE_TAG}"
 
-echo "🔨 Building Quarkus app..."
+echo "Building Quarkus app..."
 ./mvnw clean package -DskipTests
 
-echo "🐳 Building Docker image..."
+echo "Building Docker image..."
 DOCKER_DEFAULT_PLATFORM="linux/amd64" docker build \
   -f src/main/Docker/Dockerfile.jvm \
   -t ${IMAGE_URI} .
 
-echo "📤 Pushing image to Artifact Registry..."
+echo "Pushing image to Artifact Registry..."
 docker push ${IMAGE_URI}
 
-echo "🚀 Deploying to Cloud Run..."
+echo "Deploying to Cloud Run..."
 gcloud run deploy ${SERVICE_NAME} \
   --image ${IMAGE_URI} \
   --platform managed \
--- lucene-basics/gcloud.sh
+++ lucene-basics/gcloud.sh
@@ -1,3 +1,4 @@
+# pre-setup for gcloud
 gcloud auth login
 gcloud auth configure-docker europe-west6-docker.pkg.dev
 gcloud config set project gd-gcp-em-search-re-training
