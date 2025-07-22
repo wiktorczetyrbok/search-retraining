4782982c87cb4aa3eb789354cc21371f87780458
wiktorczetyrbok <kasztanators2701@gmail.com>
2025-07-22 14:52:25 +0200

    update service layer

--- clickstream/clickstream-app/src/main/java/com/griddynamics/productindexer/service/ProductService.java
+++ clickstream/clickstream-app/src/main/java/com/griddynamics/productindexer/service/ProductService.java
@@ -5,6 +5,7 @@ import com.griddynamics.productindexer.model.ProductSearchResult;
 import com.griddynamics.productindexer.repository.ProductIndexingRepository;
 import com.griddynamics.productindexer.repository.ProductSearchRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,15 +13,35 @@ import org.springframework.stereotype.Component;
 public class ProductService {
 
     private final ProductIndexingRepository productIndexingRepository;
-    private final ProductSearchRepository productSearchRepository;
+    private final ProductSearchRepository productRepository;
 
 
     public void recreateIndex() {
         productIndexingRepository.recreateIndex();
     }
 
+    @Value("${com.griddynamics.es.graduation.project.request.default.findByQuerySize}")
+    private int defaultFindByQuerySize;
+    @Value("${com.griddynamics.es.graduation.project.request.default.getAllSize}")
+    private int defaultGetAllSize;
+    @Value("${com.griddynamics.es.graduation.project.request.minQueryLength}")
+    private int minQueryLength;
+
+
     public ProductSearchResult searchProducts(ProductSearchRequest request) {
-        return productSearchRepository.searchProducts(request);
+        prepareServiceRequest(request);
+        if (request.isGetAllRequest()) {
+            return productRepository.getAllProducts(request);
+        } else if (request.getTextQuery().length() < minQueryLength) {
+            return new ProductSearchResult();
+        } else {
+            return productRepository.getProductsByQuery(request);
+        }
     }
 
+    private void prepareServiceRequest(ProductSearchRequest request) {
+        if (request.getSize() == null || request.getSize() <= 0) {
+            request.setSize(request.isGetAllRequest() ? defaultGetAllSize : defaultFindByQuerySize);
+        }
+    }
 }
